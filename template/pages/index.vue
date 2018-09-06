99e5a7560dae5730709726d8e01213daeea9379d
shoshi <shoshiroberts@gmail.com>
2018-09-06 15:38:03 -0400

    Add `PageFooter` component to base layout

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -43,6 +43,23 @@
         </div> <!-- .row -->
       </div> <!-- .wrapper -->
     </section>
+
+    <section class="sml-pad-y3 med-pad-y6">
+      <div class="wrapper">
+        <div class="row">
+          <div class="sml-c12 lrg-c8 grid-center text-center">
+            <h2>Section title goes here</h2>
+            <p class="sml-push-y2 med-push-y3">
+              Body text goes here, lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. In nibh libero, venenatis sed justo eu,
+              sollicitudin sollicitudin nisi. Integer semper tortor orci,
+              id ultricies velit laoreet in. Vestibulum sit amet ante vel risus
+              ornare ultrices sed id leo.
+            </p>
+          </div> <!-- .c -->
+        </div> <!-- .row -->
+      </div> <!-- .wrapper -->
+    </section>
   </div>
 </template>
 
