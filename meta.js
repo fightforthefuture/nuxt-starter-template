9d919fcd6ff8efc0a1d5f4003d6f4bb440856c16
Ken Mickles <ken@37i.net>
2018-09-06 23:33:44 -0400

    Add CircleCI build config to template
    And add some more config variables to the init prompts

--- meta.js
+++ meta.js
@@ -14,13 +14,34 @@ module.exports = {
       'type': 'string',
       'required': false,
       'message': 'Project description',
-      'default': 'Nuxt.js project'
+      'default': 'A Fight for the Future website'
     },
+    domainName: {
+      'type': 'string',
+      'required': false,
+      'message': 'Domain name (without www)',
+      'default': 'example.com'
+    },
+    googleAnalyticsId: {
+      'type': 'string',
+      'required': false,
+      'message': 'Google Analytics ID',
+      'default': null
+    },
+    actionNetworkPetitonId: {
+      'type': 'string',
+      'required': false,
+      'message': 'Action Network Petition ID',
+      'default': null
+    },
+    // Can't seem to make the author prompt *not* display...
     author: {
       'type': 'string',
-      'message': 'Author'
-    },
+      'required': false,
+      'message': 'Author (This isn\'t used for anything. Just hit enter...)',
+    }
   },
-  completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
+  completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}',
+  skipInterpolation: "**/*.{vue,yml}"
 };
 
